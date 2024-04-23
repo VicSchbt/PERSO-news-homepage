989b3efcdc06aaac19384f058383303821d5c6f6
VicSchbt <victoire.schubert@gmail.com>
2024-04-23 17:32:20 +0200

    :construction: ui

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-import './scss/main.scss';
+import "./scss/main.scss";
+
+const openMenuBtn = document.querySelector(".header-menu__button");
+const closeMenuBtn = document.querySelector(".menu__button-close");
+const mobileMenu = document.querySelector(".menu--mobile");
+
+openMenuBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  mobileMenu.classList.add("open");
+});
+
+closeMenuBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  mobileMenu.classList.remove("open");
+});
